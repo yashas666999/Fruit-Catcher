0d9f0b23f85af511367f607b4488b146d52ec37b
yashas666999 <108930241+yashas666999@users.noreply.github.com>
2022-11-29 18:41:34 +1000

    Add files via upload

---
+++ player.js
@@ -0,0 +1,66 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        
+        this.rank = null ;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    getPlayerAtEnd(){
+        database.ref('playerAtEnd').on("value",(data)=>{
+            this.rank = data.val();
+        })
+    }
+
+    
+
+    static updatePlayerAtEnd(rank){
+        database.ref("").update({
+            playerAtEnd : 0
+        })
+    }
+    
+    if(player.score>=5){
+        player.rank += 1;
+        updatePlayerAtEnd(player.rank);
+
+
+        player.update();
+        this.showRank();
+        gameState = 2;
+        
+    }
+
+    
+}
